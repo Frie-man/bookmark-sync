d8c18d4bfbff94fda40d528a18f3b8a61a95c286
Gerd Friemel <gerd.friemel@gmail.com>
2017-07-25 21:54:16 +0200

    implemented read-out of browser config json.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+const Sync = require('./src/sync.js');
 
 var filesDir = './text';
 
@@ -6,15 +7,14 @@ var config = __dirname + '/config.json';
 
 fs.readFile(config, 'utf8', function(err, data) {
     var config = JSON.parse(data);
-    for (var index in config["browser"]) {
+    for (var index in config["browsers"]) {
         (function(browser) {
             var browser_config_path = __dirname + '/' + browser + '.config.json';
             fs.exists(browser_config_path, function(exists) {
                 if (exists) {
-                    console.log(browser_config_path);
-                    //var $sync = require(browser_config_path);
+                    let sync = Sync(browser_config_path);
                 }
             })
-        })(config["browser"][index])
+        })(config["browsers"][index])
     }
 })
--- src/sync.js
+++ src/sync.js
@@ -1,12 +1,15 @@
-
+var fs = require('fs');
 var sync_job = {};
 
-var
-
-function sync() {
-
+function Sync(config_file_path) {
+    let that = this;
+    
+    fs.readFile(config_file_path, 'utf8', function(err, data) {
+        that.config = JSON.parse(data);
+        console.log(data);
+    })
 }
 
 
 
-module.exports = sync;
+module.exports = Sync;
